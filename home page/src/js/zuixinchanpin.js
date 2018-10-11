e31a72f9d2314cc2d438cd12dcaa9686d3d3808d
zhangjian <710678692@qq.com>
2018-10-11 20:43:49 +0800

    最后一次提交-张坚

--- home page/src/js/zuixinchanpin.js
+++ home page/src/js/zuixinchanpin.js
@@ -16,19 +16,13 @@ window.onload=function(){
         });
     },3000)
     
-
-//   $('#id').animate({
-//     top:34,
-//     opacity:1,
-// },1000)
-//     setInterval(function(){
-// $('.changing_one').eq(i).children('img').animate({
-//             top:34,
-//             opacity:1,
-//         },1000)
-//         $('.changing_one').eq(i-1).siblings().children('img').css({
-//             top:76,
-//             opacity:0
-//         });
-//     },3000)
 }
+$(function(){
+    $('.tab_top .ul2 li').on('click', function() {
+        var i = $(this).index()
+        $(this).addClass('tim').siblings().removeClass('tim');
+        // $('li').eq(i).show().siblings().hide();//css中的.ccc{的！important需要移除}
+        $('.max_contain').eq(i).addClass('xxx').siblings().removeClass('xxx'); //css中的.ccc{的！important需要在}
+
+    })
+})
