64486772de6fbd611e0788c258c8abc06d052c6c
chenqifu <2371400356@qq.com>
2018-10-11 16:00:48 +0800

    'chenqifu'

--- home page/src/js/servers.js
+++ home page/src/js/servers.js
@@ -1,12 +1,12 @@
 $('.often_servers1').mouseover(function(){
     $('#tend').css({
-        'top':'30px','left':'40%','width':'36px','height':'36px','transition':'all 0.5s'
+        'top':'30px','left':'40%','width':'36px','height':'36px','transition':'all 0.01s'
     });
     $('#tend_h4').css({
-        'top':'40px','transition':'all 0.5s'
+        'top':'40px','transition':'all 0.01s'
     });
     $('#sigin').css({
-        'top':'50px','transition':'all 0.5s'
+        'top':'50px','transition':'all 0.01s'
     });
 });
 $('.often_servers1').mouseout(function(){
@@ -23,13 +23,13 @@ $('.often_servers1').mouseout(function(){
 // ========================
 $('.often_servers2').mouseover(function(){
     $('#tend2').css({
-        'width':'36px','height':'36px','top':'30px','left':'40%','transition':'all 0.5s'
+        'width':'36px','height':'36px','top':'30px','left':'40%','transition':'all 0.01s'
     });
     $('#tend_h2').css({
-        'top':'40px','transition':'all 0.5s'
+        'top':'40px','transition':'all 0.01s'
     });
     $('#sigin2').css({
-        'top':'50px','transition':'all 0.5s'
+        'top':'50px','transition':'all 0.01s'
     });
 });
 $('.often_servers2').mouseout(function(){
@@ -46,13 +46,13 @@ $('.often_servers2').mouseout(function(){
 // 
 $('.often_servers3').mouseover(function(){
     $('#tend3').css({
-        'width':'36px','height':'36px','top':'30px','left':'40%','transition':'all 0.5s'
+        'width':'36px','height':'36px','top':'30px','left':'40%','transition':'all 0.01s'
     });
     $('#tend_h3').css({
-        'top':'40px','transition':'all 0.5s'
+        'top':'40px','transition':'all 0.01s'
     });
     $('#sigin3').css({
-        'top':'50px','transition':'all 0.5s'
+        'top':'50px','transition':'all 0.01s'
     });
 });
 $('.often_servers3').mouseout(function(){
@@ -69,13 +69,13 @@ $('.often_servers3').mouseout(function(){
 // 
 $('.often_servers4').mouseover(function(){
     $('#tend4').css({
-        'width':'36px','height':'36px','top':'30px','left':'40%','transition':'all 0.5s'
+        'width':'36px','height':'36px','top':'30px','left':'40%','transition':'all 0.01s'
     });
     $('#tend_h5').css({
-        'top':'40px','transition':'all 0.5s'
+        'top':'40px','transition':'all 0.01s'
     });
     $('#sigin4').css({
-        'top':'50px','transition':'all 0.5s'
+        'top':'50px','transition':'all 0.01s'
     });
 });
 $('.often_servers4').mouseout(function(){
@@ -92,13 +92,13 @@ $('.often_servers4').mouseout(function(){
 // 
 $('.often_servers6').mouseover(function(){
     $('#tend5').css({
-        'width':'36px','height':'36px','top':'30px','left':'40%','transition':'all 0.5s'
+        'width':'36px','height':'36px','top':'30px','left':'40%','transition':'all 0.01s'
     });
     $('#tend_h6').css({
-        'top':'40px','transition':'all 0.5s'
+        'top':'40px','transition':'all 0.01s'
     });
     $('#sigin6').css({
-        'top':'50px','transition':'all 0.5s'
+        'top':'50px','transition':'all 0.01s'
     });
 });
 $('.often_servers6').mouseout(function(){
@@ -115,13 +115,13 @@ $('.often_servers6').mouseout(function(){
 // 
 $('.often_servers7').mouseover(function(){
     $('#tend6').css({
-        'width':'36px','height':'36px','top':'30px','left':'40%','transition':'all 0.5s'
+        'width':'36px','height':'36px','top':'30px','left':'40%','transition':'all 0.01s'
     });
     $('#tend_h7').css({
-        'top':'40px','transition':'all 0.5s'
+        'top':'40px','transition':'all 0.01s'
     });
     $('#sigin7').css({
-        'top':'50px','transition':'all 0.5s'
+        'top':'50px','transition':'all 0.01s'
     });
 });
 $('.often_servers7').mouseout(function(){
