085f7cc28a8fc9e350329f6ee5fbaccb26dcce30
FangFangZhenZhen <851580041@qq.com>
2018-10-13 08:55:22 +0800

    2018.10.13-fangzhen

--- home page/src/js/guide.js
+++ home page/src/js/guide.js
@@ -1,31 +1,32 @@
 // =========视频教程划入显示播放=========
-$('.view_1').mouseover(function(){
-    $('#play1').show();
-});
-$('.view_1').mouseout(function(){
-    $('#play1').hide();
-});
+// $('.view_1').mouseover(function(){
+//     $('#play1').show();
+
+// });
+// $('.view_1').mouseout(function(){
+//     $('#play1').hide();
+// });
 // ----------------
-$('.view_2').mouseover(function(){
-    $('#play2').show();
-});
-$('.view_2').mouseout(function(){
-    $('#play2').hide();
-});
-// --------------
-$('.view_3').mouseover(function(){
-    $('#play3').show();
-});
-$('.view_3').mouseout(function(){
-    $('#play3').hide();
-});
-//------- 
-$('.view_4').mouseover(function(){
-    $('#play4').show();
-});
-$('.view_4').mouseout(function(){
-    $('#play4').hide();
-});
+// $('.view_2').mouseover(function(){
+//     $('#play2').show();
+// });
+// $('.view_2').mouseout(function(){
+//     $('#play2').hide();
+// });
+// // --------------
+// $('.view_3').mouseover(function(){
+//     $('#play3').show();
+// });
+// $('.view_3').mouseout(function(){
+//     $('#play3').hide();
+// });
+// //------- 
+// $('.view_4').mouseover(function(){
+//     $('#play4').show();
+// });
+// $('.view_4').mouseout(function(){
+//     $('#play4').hide();
+// });
 // ---------------------------------------------------------
 
 
